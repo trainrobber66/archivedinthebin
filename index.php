f5cdaea16de2d2033934b8596f1e68cb2d4438c6
trainrobber66 <67712195+trainrobber66@users.noreply.github.com>
2020-12-28 08:45:44 +0000

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "GET") {
-  // do logic
+  echo 'done!';
+  echo '<a href="http://www.website.com/page.html">go back</a>';
+   // do logic
   $com = $_GET['com'];
 }
 ?>
